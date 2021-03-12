aeb0880e39209529defc912e1b90453b04135df9
kyson zhu <zjh171@qq.com>
2021-03-12 22:44:08 +0800

    [add] change to usercenter vc

--- GJTApplicationModule.podspec
+++ GJTApplicationModule.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GJTApplicationModule'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'Gojito 的启动器业务模块'
   s.platform         = :ios
 
@@ -39,5 +39,6 @@ Pod::Spec.new do |s|
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-   s.dependency 'GJTAppLauncher'
+  s.dependency 'GJTAppLauncher'
+  s.dependency 'GJTMvvmKit'
 end
--- GJTApplicationModule/Classes/GJTAppLauncherDelegate+TailTask.m
+++ GJTApplicationModule/Classes/GJTAppLauncherDelegate+TailTask.m
@@ -7,12 +7,9 @@
 
 #import "GJTAppLauncherDelegate+TailTask.h"
 #import <GJTAppLauncher/GJTAppLauncher.h>
+#import <GJTMvvmKit/GJTMvvmKit.h>
 
-//#import <GJTMineModule/GJTMineModule.h>
-//#import <GJTMomentModule/GJTDiscoveryViewController.h>
-//#import <GJTGroupModule/GJTHomeViewController.h>
-//#import <GJTMvvmKit/GJTMvvmKit.h>
-
+@class GJTMineViewController;
 
 @implementation GJTAppLauncherDelegate(TailTask)
 
@@ -22,22 +19,15 @@
 
 +(void) setupHomeViewController {
     
-//    GJTMineViewController *mineVC = [[GJTMineViewController alloc] init];
-//    GJTDiscoveryViewController *discoveryVC = [[GJTDiscoveryViewController alloc] init];
-//    GJTHomeViewController *homeVC = [[GJTHomeViewController alloc] init];
-//
-//    GJTNavigationController *navc1 = [[GJTNavigationController alloc] initWithRootViewController:homeVC];
-//    GJTNavigationController *navc2 = [[GJTNavigationController alloc] initWithRootViewController:discoveryVC];
-//
-//    GJTNavigationController *navc4 = [[GJTNavigationController alloc] initWithRootViewController:mineVC];
-//
-//    UITabBarController *tabController = [[UITabBarController alloc] init];
-//    tabController.viewControllers = @[navc1,navc2,navc4];
-//
-//    UIWindow *rootWindow = ((GJTAppLauncherDelegate *)([UIApplication sharedApplication].delegate)).window;
-//    rootWindow.rootViewController = tabController;
-//
-//    [rootWindow makeKeyAndVisible];
+    Class targetClass = NSClassFromString(@"GJTMineViewController");
+    id mineVC = [[targetClass alloc] init];
+    GJTNavigationController *navc4 = [[GJTNavigationController alloc] initWithRootViewController:mineVC];
+    UITabBarController *tabController = [[UITabBarController alloc] init];
+    tabController.viewControllers = @[navc4];
+
+    UIWindow *rootWindow = ((GJTAppLauncherDelegate *)([UIApplication sharedApplication].delegate)).window;
+    rootWindow.rootViewController = tabController;
+    [rootWindow makeKeyAndVisible];
 }
 
 @end
